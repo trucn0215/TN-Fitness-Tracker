c76bf65ac11ce50a93feab46bcdc124c27f8c96d
Truc Nguyen <trucn0215@gmail.com>
2021-02-09 00:19:42 -0800

    require the html routes to be able route homepage, exercise and stats

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,19 @@
 const path = require("path");
+const router = require("express").Router();
 
-module.exports = function(app){
-    app.get("/", function (req, res) {
+    // Homepage routes
+    router.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"))
-    })
-}
+    });
+
+    // Exercise routes
+    router.get("/exercise", (req, res)=> {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    });
+
+    // Status routes
+    router.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
+    });
+
+module.exports = router;
