03c5937bac84b8aeef41cca7b5fa4347964408aa
Truc Nguyen <trucn0215@gmail.com>
2021-02-08 17:56:24 -0800

    create get, put, post routes in apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+router.get("/api/workouts", (req, res) => {
+
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+
+});
+
+router.post("/api/workouts", (req, res) => {
+    
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    
+});
