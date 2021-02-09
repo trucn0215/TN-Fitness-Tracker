16196324a59b48b564249bfadbdda33b7199dcce
Truc Nguyen <trucn0215@gmail.com>
2021-02-08 18:02:23 -0800

    create Workout.js models

---
+++ models/Workout.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: Date,
+    exercises: [
+        {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number
+        }
+    ]
+})
